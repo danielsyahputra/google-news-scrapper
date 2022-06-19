411ccd2518ac8c393b88fa6191c49c921180da84
danielsyahputra13 <dnlshp@gmail.com>
2022-06-19 12:17:29 +0700

    add chromedriver settings

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -11,7 +11,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
@@ -84,7 +83,7 @@ def load_driver():
     # op = webdriver.ChromeOptions()
     # op.add_argument('headless')
     # driver = webdriver.Chrome(driver_path, options=op)
-    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    driver = webdriver.Chrome(ChromeDriverManager().install()) 
     return driver
 
 def to_excel(df):
